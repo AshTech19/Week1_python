7bb3f0a7a3349a81a7570def5a0b7805571e6d08
AshTech19 <ayesha19ab@gmail.com>
2021-05-22 02:33:35 +0530

    Initial commit

---
+++ Week1/CalcAreaOfCircle.py
@@ -0,0 +1,5 @@
+import math
+
+radius=float(input("Enter radius of circle: "))       # Accept radius from user
+area=math.pi * radius * radius                        # Calculate
+print(f'Area of circle of radius {radius} = {area}')  # Display output
---
+++ Week1/CurrentDateTime.py
@@ -0,0 +1,5 @@
+import datetime
+
+current = datetime.datetime.now()  # Calling from module
+print("  Date    |     Time ")
+print(str(current))
